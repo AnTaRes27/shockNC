08a24681cffeab2d625a632ef2ea42f8f4c5ad12
AnTaRes27 <39043966+AnTaRes27@users.noreply.github.com>
2024-02-10 20:20:31 -0600

    add signal encoding comments

--- src/main.cpp
+++ src/main.cpp
@@ -49,32 +49,49 @@ void loop()
   uint8_t command[14];
   uint8_t x = 0;
 
-  // synchronization
+  // synchronization (6 bytes)
+  // this is always constant
   command[0] = 0x3a;
   command[1] = 0x97;
   command[2] = 0xb2;
   command[3] = 0x59;
   command[4] = 0xc7;
   command[5] = 0x4f;
-  // key
+
+  // key (2 bytes)
+  // this is unique per transmitter. arbitrary
   command[6] = 0x53;
   command[7] = 0xc5;
-  // constant1
-  command[8] = 0b1010 << 4;
-  // channel
-  command[8] = 0b0101 | command[8];
-  // rolling code
+
+  // constant1 (4 bits)
+  // this is always constant
+  command[8] = 0x0a << 4;
+
+  // channel (4 bits)
+  // 4 different options: 0x5, 0x6, 0x7, 0x8,
+  command[8] = 0x05 | command[8];
+
+  // rolling code (1 byte)
+  // 0-255, increment by 1 each time
   command[9] = rolling_code++;
-  // constant2
-  command[10] = 0b0111 << 4;
-  // function
-  command[10] = 0b0010 | command[10];
-  // strength
+
+  // constant2 (4 bits)
+  // this is always constant
+  command[10] = 0x07 << 4;
+
+  // function (4 bits)
+  // shock: 0x01
+  // buzz: 0x02
+  // beep: 0x03
+  command[10] = 0x02 | command[10];
+
+  // strength (1 byte)
+  // shock: 1-99
+  // buzz: 1-9
   command[11] = 5;
 
   // checksum
-  // calculated_crc = crc.add;
-
+  // check CRC parameters above
   crc.restart();
   for (uint8_t i = 0; i < 12; i++)
   {
@@ -93,6 +110,7 @@ void loop()
   Serial.print(calculated_crc, HEX);
   Serial.print(' ');
 
+  // transmit
   Serial.println(radio.transmit(command, 14));
 
   delay(500);
